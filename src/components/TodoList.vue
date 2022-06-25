f38c82090c6b96cdf15d4572f9079f6b87836bae
deem-zee <zimindmitri@gmail.com>
2022-06-25 17:32:38 +0300

    стилизация под несколько разрешений

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -125,7 +125,6 @@ export default {
     created() {
         axios.get('https://todolist-e0681-default-rtdb.europe-west1.firebasedatabase.app/todolist.json')
         .then(response => {
-           console.log(response.data)
            return response.data 
         } 
             
@@ -151,14 +150,380 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&family=Comfortaa:wght@300;400;500;600&display=swap');
 @media screen and (max-width: 360px){
+     .container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    border: 1px solid grey;
+    background: rgb(240, 235, 235);
+    position: relative;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.btnBlock {
+    position: absolute;
+    top: 80px;
+    /* left: 440px; */
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+}
+
+.btnBlock :hover {
+    cursor: pointer;
     
 }
 
-@media screen and (max-width: 768px){
+.btnAll {
+    height: 30px;
+    /* border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px; */
+    border: 1px solid white;
+    background: rgb(18, 223, 114);
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.btnPrgrs {
+    height: 30px;
+    border: 1px solid white;
+    background: rgb(18, 223, 114);
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.btnComp {
+    height: 30px;
+    border: 1px solid white;
+    background: rgb(18, 223, 114);
+    /* border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px; */
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.container h1 {
+    height: 200px;
+    background: rgb(240, 235, 235);
+    margin-bottom: 0;
+    /* border-top: 1px solid grey; */
+    border-bottom: 1px solid grey;
+}
+
+
+.newTask {
+    transform: scale(0.6);
+    box-sizing: border-box;
+    /* padding: 5px 5px; */
+    border-radius: 50%;
+    border: none;
+    background: rgb(18, 223, 114);
+    width: 70px;
+    height: 4rem;
+    font-size: 2em;
+    align-self: center;
+    overflow: hidden;
+    position: absolute;
+    top: 190px;
+    right: 50px;
+    color: white;
+
+}
+
+.newTask:hover {
+    cursor: pointer;
+}
+
+.inputBlock {
+    
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+    margin: 10px 10px;
+}
+
+.taskInput {
+    transform: scale(0.5);
+    margin: 10px;
+    height: 80px;
+    width: 40rem;
+    border-radius: 10px;
+    padding-left: 20px;
+    font-size: 20px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.addTask {
+    width: 150px;
+    height: 40px;
+    border-radius: 5em;
+    background: rgb(18, 223, 114);
+    margin: 5px;
+    border: none;
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+    color: white;
+}
+
+.addTask:hover {
+    background: rgb(0, 244, 114);
+}
+
+
+.cancel {
+    display: block;
+    position: relative;
+    left: 265px;
+    width: 150px;
+    height: 40px;
+    border-radius: 5em;
+    background: rgb(235, 44, 44);
+    margin: 5px;
+    border: none;
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+    color: white;
+}
+
+.cancel:hover {
+    background: rgb(253, 2, 2);
+}
+
+.btn {
+    width: 100px;
+    height: 40px;
+    border-radius: 5em;
+    background: rgb(18, 223, 114);
+    margin: 5px;
+}
+
+.allTasks, .completedTasks, .inprogressTasks {
+    background: rgb(249, 247, 247);
+}
+
+ol {
+    padding: 0;
+    list-style-type: none;
+    margin: 0;
+}
+
+
+
+ li {
+    font-size: 16px;
+    padding: 10px;
+    height: 50px;
+    border-bottom: 1px solid grey;
+    width: 100%;
+    text-align: center;
+    padding-top: 30px;
+}
+
+li:last-child {
+    border-bottom: none;
+}
+
+.allTasks li:hover {
+    cursor: pointer;
+}
+}
+
+@media screen and (min-width: 361px) and (max-width: 768px){
+         .container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    border: 1px solid grey;
+    background: rgb(240, 235, 235);
+    position: relative;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.btnBlock {
+    position: absolute;
+    top: 80px;
+    /* left: 440px; */
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+}
+
+.btnBlock :hover {
+    cursor: pointer;
     
 }
 
-@media screen and (min-width: 768px) and (max-width: 2500px){
+.btnAll {
+    height: 30px;
+    /* border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px; */
+    border: 1px solid white;
+    background: rgb(18, 223, 114);
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.btnPrgrs {
+    height: 30px;
+    border: 1px solid white;
+    background: rgb(18, 223, 114);
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.btnComp {
+    height: 30px;
+    border: 1px solid white;
+    background: rgb(18, 223, 114);
+    /* border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px; */
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.container h1 {
+    height: 200px;
+    background: rgb(240, 235, 235);
+    margin-bottom: 0;
+    /* border-top: 1px solid grey; */
+    border-bottom: 1px solid grey;
+}
+
+
+.newTask {
+    transform: scale(0.6);
+    box-sizing: border-box;
+    /* padding: 5px 5px; */
+    border-radius: 50%;
+    border: none;
+    background: rgb(18, 223, 114);
+    width: 70px;
+    height: 4rem;
+    font-size: 2em;
+    align-self: center;
+    overflow: hidden;
+    position: absolute;
+    top: 190px;
+    right: 110px;
+    color: white;
+
+}
+
+.newTask:hover {
+    cursor: pointer;
+}
+
+.inputBlock {
+    
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+    margin: 10px 10px;
+}
+
+.taskInput {
+    transform: scale(0.5);
+    margin: 10px;
+    height: 80px;
+    width: 40rem;
+    border-radius: 10px;
+    padding-left: 20px;
+    font-size: 20px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+}
+
+.addTask {
+    width: 150px;
+    height: 40px;
+    border-radius: 5em;
+    background: rgb(18, 223, 114);
+    margin: 5px;
+    border: none;
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+    color: white;
+}
+
+.addTask:hover {
+    background: rgb(0, 244, 114);
+}
+
+
+.cancel {
+    display: block;
+    position: relative;
+    left: 265px;
+    width: 150px;
+    height: 40px;
+    border-radius: 5em;
+    background: rgb(235, 44, 44);
+    margin: 5px;
+    border: none;
+    font-size: 18px;
+    font-family: 'Be Vietnam Pro', sans-serif;
+    font-family: 'Comfortaa', cursive;
+    color: white;
+}
+
+.cancel:hover {
+    background: rgb(253, 2, 2);
+}
+
+.btn {
+    width: 100px;
+    height: 40px;
+    border-radius: 5em;
+    background: rgb(18, 223, 114);
+    margin: 5px;
+}
+
+.allTasks, .completedTasks, .inprogressTasks {
+    background: rgb(249, 247, 247);
+}
+
+ol {
+    padding: 0;
+    list-style-type: none;
+    margin: 0;
+}
+
+
+
+ li {
+    font-size: 16px;
+    padding: 10px;
+    height: 50px;
+    border-bottom: 1px solid grey;
+    width: 100%;
+    text-align: center;
+    padding-top: 30px;
+}
+
+li:last-child {
+    border-bottom: none;
+}
+
+.allTasks li:hover {
+    cursor: pointer;
+}
+}
+
+@media screen and (min-width: 769px) and (max-width: 2500px){
     .container {
     display: flex;
     justify-content: center;
